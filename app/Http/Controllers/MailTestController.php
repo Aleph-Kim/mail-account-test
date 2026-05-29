48834148c7a94a2964d4811e2b42930782a9601a
Aleph kim <dktjdej@naver.com>
2026-05-29 15:57:07 +0900

    refactor: MailTestController 코드 정렬

--- app/Http/Controllers/MailTestController.php
+++ app/Http/Controllers/MailTestController.php
@@ -16,14 +16,14 @@ class MailTestController extends Controller
     public function index(Request $request)
     {
         $defaults = [
-            'mailer'       => $request->cookie('mail_mailer',       config('mail.default', 'smtp')),
-            'host'         => $request->cookie('mail_host',         config('mail.mailers.smtp.host', '')),
-            'port'         => $request->cookie('mail_port',         config('mail.mailers.smtp.port', 465)),
-            'username'     => $request->cookie('mail_username',     config('mail.mailers.smtp.username', '')),
-            'password'     => $request->cookie('mail_password',     config('mail.mailers.smtp.password', '')),
-            'encryption'   => $request->cookie('mail_encryption',   config('mail.mailers.smtp.encryption', 'ssl')),
+            'mailer' => $request->cookie('mail_mailer', config('mail.default', 'smtp')),
+            'host' => $request->cookie('mail_host', config('mail.mailers.smtp.host', '')),
+            'port' => $request->cookie('mail_port', config('mail.mailers.smtp.port', 465)),
+            'username' => $request->cookie('mail_username', config('mail.mailers.smtp.username', '')),
+            'password' => $request->cookie('mail_password', config('mail.mailers.smtp.password', '')),
+            'encryption' => $request->cookie('mail_encryption', config('mail.mailers.smtp.encryption', 'ssl')),
             'from_address' => $request->cookie('mail_from_address', config('mail.from.address', '')),
-            'from_name'    => $request->cookie('mail_from_name',    config('mail.from.name', '')),
+            'from_name' => $request->cookie('mail_from_name', config('mail.from.name', '')),
         ];
 
         return view('mail-test', compact('defaults'));
@@ -34,14 +34,14 @@ class MailTestController extends Controller
         $minutes = 60 * 24 * 365;
 
         return response()->json(['ok' => true])
-            ->withCookie(cookie('mail_mailer',       $request->input('mailer', ''),       $minutes))
-            ->withCookie(cookie('mail_host',         $request->input('host', ''),         $minutes))
-            ->withCookie(cookie('mail_port',         $request->input('port', ''),         $minutes))
-            ->withCookie(cookie('mail_username',     $request->input('username', ''),     $minutes))
-            ->withCookie(cookie('mail_password',     $request->input('password', ''),     $minutes))
-            ->withCookie(cookie('mail_encryption',   $request->input('encryption', ''),   $minutes))
+            ->withCookie(cookie('mail_mailer', $request->input('mailer', ''), $minutes))
+            ->withCookie(cookie('mail_host', $request->input('host', ''), $minutes))
+            ->withCookie(cookie('mail_port', $request->input('port', ''), $minutes))
+            ->withCookie(cookie('mail_username', $request->input('username', ''), $minutes))
+            ->withCookie(cookie('mail_password', $request->input('password', ''), $minutes))
+            ->withCookie(cookie('mail_encryption', $request->input('encryption', ''), $minutes))
             ->withCookie(cookie('mail_from_address', $request->input('from_address', ''), $minutes))
-            ->withCookie(cookie('mail_from_name',    $request->input('from_name', ''),    $minutes));
+            ->withCookie(cookie('mail_from_name', $request->input('from_name', ''), $minutes));
     }
 
     public function send(SendMailRequest $request)
